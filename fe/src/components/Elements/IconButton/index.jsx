f23f3101706cf2375f3e8bef5ef6e31bb18cdcc7
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-10 13:53:05 +0700

    add detailpage

--- fe/src/components/Elements/IconButton/index.jsx
+++ fe/src/components/Elements/IconButton/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { IconButton } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
-const IconColor = ({ children }) => {
+const IconColor = ({ children, color = 'teal' }) => {
   return (
-    <IconButton aria-label="Call support" rounded="full" colorPalette={'teal'}>
+    <IconButton aria-label="Call support" rounded="full" colorPalette={color}>
       {children}
     </IconButton>
   );
 };
 IconColor.propTypes = {
   children: PropTypes.node,
+  color: PropTypes.string,
 };
 
 export default IconColor;
--- fe/src/pages/DetailMentorpage.jsx
+++ fe/src/pages/DetailMentorpage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Layouts from '../components/Layouts/Layouts';
-import { Box } from '@chakra-ui/react';
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { mentors } from '../utils/mentors';
+import { Grid, GridItem, Box, Flex, Text } from '@chakra-ui/react';
+import CardBox from '../components/Fragments/CardBox';
+import IconColor from '../components/Elements/IconButton';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { IoChatbubbleEllipsesSharp } from 'react-icons/io5';
+import { MdOutlineAccessTimeFilled } from 'react-icons/md';
 const DetailMentorpage = () => {
   const { mentorId } = useParams();
   const [detailMentor, setDetailMentor] = useState(null);
@@ -14,8 +19,44 @@ const DetailMentorpage = () => {
   }, [mentorId]);
   return (
     <>
-      <Box>
-        <Layouts>DetailMentor</Layouts>
+      <Box backgroundColor="textGreen">
+        <Layouts>
+          <Grid templateColumns={'repeat(3, 1fr)'}>
+            <GridItem colSpan={2} border={'1px solid #ccc'}>
+              s
+            </GridItem>
+            <GridItem colSpan={1} position={'relative'}>
+              <Box position={'sticky'}>
+                <CardBox>
+                  <CardBox.Body
+                    title="Rp. 100.000/Session"
+                    description="Cocok untuk mereka yang ingin melakukan perubahan karier, mendapatkan promosi, atau membutuhkan bimbingan/arahan karier"
+                  />
+                  <Box>
+                    <Flex columnGap={3} pl={3} alignItems={'center'}>
+                      <IconColor color="white">
+                        <BsFillTelephoneFill color="teal" />
+                      </IconColor>
+                      <Text fontSize="sm">Regular calls, per agreement</Text>
+                    </Flex>
+                    <Flex columnGap={3} pl={3} alignItems={'center'}>
+                      <IconColor color="white">
+                        <IoChatbubbleEllipsesSharp color="teal" />
+                      </IconColor>
+                      <Text fontSize="sm">Unlimited Q&A via chat</Text>
+                    </Flex>
+                    <Flex columnGap={3} pl={3} alignItems={'center'}>
+                      <IconColor color="white">
+                        <MdOutlineAccessTimeFilled color="teal" />
+                      </IconColor>
+                      <Text fontSize="sm">Expect responses in 3-4 days</Text>
+                    </Flex>
+                  </Box>
+                </CardBox>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Layouts>
       </Box>
     </>
   );
