f45ff5e681d27af1a4a30d6146047854ece29ef8
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-10 23:44:38 +0700

    add detailpage

---
+++ fe/src/components/Fragments/Breadcrumb.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Breadcrumb, Span, Flex } from '@chakra-ui/react';
+import { LuHouse, LuShirt } from 'react-icons/lu';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+
+const BreadcrumbLink = ({ mentorName }) => {
+  return (
+    <Breadcrumb.Root>
+      <Breadcrumb.List color="#ffff">
+        <Breadcrumb.Item>
+          <Link href="/">
+            <Flex columnGap={2} alignItems={'center'} _hover={{ color: '#ccc' }}>
+              <LuHouse />
+              Home
+            </Flex>
+          </Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator color="#ffff" />
+
+        <Breadcrumb.Item>
+          <Link href="/mentors">
+            <Flex columnGap={2} alignItems={'center'} _hover={{ color: '#ccc' }}>
+              <LuShirt />
+              Find a Mentor
+            </Flex>
+          </Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator color="#ffff" />
+
+        <Breadcrumb.Item>
+          <Breadcrumb.CurrentLink>{mentorName}</Breadcrumb.CurrentLink>
+        </Breadcrumb.Item>
+      </Breadcrumb.List>
+    </Breadcrumb.Root>
+  );
+};
+export default BreadcrumbLink;
