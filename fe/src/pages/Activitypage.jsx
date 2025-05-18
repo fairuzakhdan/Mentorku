0289a9a605ccd52e05b2a9a02c6cb9bb9f7b24bd
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-18 11:39:08 +0700

    add activity page

---
+++ fe/src/pages/Activitypage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigation from '../components/Layouts/Navigation';
+
+const Activitypage = () => {
+  return <Navigation type="sidebar" />;
+};
+
+export default Activitypage;
