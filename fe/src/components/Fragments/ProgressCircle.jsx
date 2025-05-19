790820c73c1938eaa09e3cd27f9950fe06c5873b
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-19 14:05:20 +0700

    updated

--- fe/src/components/Fragments/ProgressCircle.jsx
+++ fe/src/components/Fragments/ProgressCircle.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { ProgressCircle } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
-
-const Progress = ({value}) => {
+const Progress = ({ value }) => {
   return (
-    <ProgressCircle.Root value={75}>
+    <ProgressCircle.Root value={value}>
       <ProgressCircle.Circle>
         <ProgressCircle.Track />
         <ProgressCircle.Range strokeLinecap="round" />
       </ProgressCircle.Circle>
     </ProgressCircle.Root>
   );
+};
+Progress.propTypes = {
+  value: PropTypes.number,
 };
 export default Progress;
