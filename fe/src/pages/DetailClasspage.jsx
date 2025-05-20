448797e73094adb2b4bcb6ecb40cff6332df261e
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-20 23:44:07 +0700

    add class page

---
+++ fe/src/pages/DetailClasspage.jsx
@@ -0,0 +1,9 @@
+import Navigation from "../components/Layouts/Navigation";
+
+const DetailClasspage = () => {
+    return (
+        <>
+            <Navigation type="navbar" />
+        </>
+    );
+};
