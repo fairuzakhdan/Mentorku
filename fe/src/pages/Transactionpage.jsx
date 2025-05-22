5f7527e9e5ff80cb575ad4f4fd3aa47c94bdb689
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-22 10:29:45 +0700

    add transactionpage

---
+++ fe/src/pages/Transactionpage.jsx
@@ -0,0 +1,9 @@
+import Navigation from '../components/Layouts/Navigation';
+const Transactionpage = () => {
+  return (
+    <>
+      <Navigation type="sidebar"></Navigation>
+    </>
+  );
+};
+export default Transactionpage;
