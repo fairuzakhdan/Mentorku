07640fce25989298aa33cb829c81ac4b46ba7464
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-24 13:59:03 +0700

    add transactionpage

--- fe/src/pages/Blog.jsx
+++ fe/src/pages/Blog.jsx
@@ -23,7 +23,7 @@ const Blogpage = () => {
     {
       id: '1',
       title: 'Software Rekomendasi Pembuat Aplikasi Android Terbaik',
-      category: 'Web Dev',
+      category: 'Mobile Dev',
       createdAt: '01 Januari 2025',
       image:
         'https://images.unsplash.com/photo-1618388810903-840bb0d15ea5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D',
@@ -52,6 +52,14 @@ const Blogpage = () => {
       image:
         'https://plus.unsplash.com/premium_photo-1664301973841-fe2da7427d6c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwcm9ncmFtbWVyfGVufDB8fDB8fHww',
     },
+    {
+      id: '5',
+      title: 'Tips Membuat Website Agar Banyak Pengunjung',
+      category: 'Tips & Trick',
+      createdAt: '05 May 2025',
+      image:
+        'https://plus.unsplash.com/premium_photo-1664301973841-fe2da7427d6c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwcm9ncmFtbWVyfGVufDB8fDB8fHww',
+    },
   ];
   return (
     <Layouts>
@@ -59,8 +67,8 @@ const Blogpage = () => {
         <TabsLink
           tabs={[
             {
-              label: 'Website',
-              value: 'web',
+              label: 'All',
+              value: 'all',
               content: (
                 <Flex gap={5} flexWrap={'wrap'} justifyContent={'space-between'} mt={5}>
                   {blogs.map((blog, index) => (
@@ -72,6 +80,63 @@ const Blogpage = () => {
                       image={blog.image}
                     />
                   ))}
+                </Flex>
+              ),
+            },
+            {
+              label: 'Website Development',
+              value: 'web',
+              content: (
+                <Flex gap={5} flexWrap={'wrap'} justifyContent={'space-between'} mt={5}>
+                  {blogs
+                    .filter((blog) => blog.category === 'Web Dev')
+                    .map((blog, index) => (
+                      <ContentBlog
+                        key={index}
+                        title={blog.title}
+                        createdAt={blog.createdAt}
+                        category={blog.category}
+                        image={blog.image}
+                      />
+                    ))}
+                </Flex>
+              ),
+            },
+            {
+              label: 'Mobile Development',
+              value: 'mobile',
+              content: (
+                <Flex gap={5} flexWrap={'wrap'} justifyContent={'space-between'} mt={5}>
+                  {blogs
+                    .filter((blog) => blog.category === 'Mobile Dev')
+                    .map((blog, index) => (
+                      <ContentBlog
+                        key={index}
+                        title={blog.title}
+                        createdAt={blog.createdAt}
+                        category={blog.category}
+                        image={blog.image}
+                      />
+                    ))}
+                </Flex>
+              ),
+            },
+            {
+              label: 'Tips and Trick',
+              value: 'tips',
+              content: (
+                <Flex gap={5} flexWrap={'wrap'} justifyContent={'space-between'} mt={5}>
+                  {blogs
+                    .filter((blog) => blog.category === 'Tips & Trick')
+                    .map((blog, index) => (
+                      <ContentBlog
+                        key={index}
+                        title={blog.title}
+                        createdAt={blog.createdAt}
+                        category={blog.category}
+                        image={blog.image}
+                      />
+                    ))}
                 </Flex>
               ),
             },
