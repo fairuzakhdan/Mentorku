84055c91987dc6f4665c182cb53d9a6b7ce17639
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-24 20:19:27 +0700

    add transactionpage

--- fe/src/pages/DetailBlogpage.jsx
+++ fe/src/pages/DetailBlogpage.jsx
@@ -1,7 +1,11 @@
 import Layouts from '../components/Layouts/Layouts';
 import { Box, Grid, GridItem, Text, Image, Button } from '@chakra-ui/react';
 const DetailBlogpage = () => {
-  return <Layouts></Layouts>;
+  return (
+    <Layouts>
+      <Box mt={3}></Box>
+    </Layouts>
+  );
 };
 
 export default DetailBlogpage;
