1104214eff8161ef250f939d145674b80c8ac054
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-24 22:01:21 +0700

    add transactionpage

--- fe/src/pages/JoinUspage.jsx
+++ fe/src/pages/JoinUspage.jsx
@@ -1,5 +1,11 @@
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import Layouts from '../components/Layouts/Layouts';
 const JoinUspage = () => {
+  return (
+    <Box backgroundColor="textGreen">
+      <Layouts></Layouts>
+    </Box>
+  );
+};
 
-}
-
-export default JoinUspage
+export default JoinUspage;
