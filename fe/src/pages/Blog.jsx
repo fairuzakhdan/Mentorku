ab6c672e17c49292422ec9e96d10a57b200915f9
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-24 20:21:08 +0700

    add transactionpage

--- fe/src/pages/Blog.jsx
+++ fe/src/pages/Blog.jsx
@@ -22,48 +22,7 @@ const ContentBlog = ({ image, createdAt, title, category }) => {
 
 const Blogpage = () => {
   const [blogs, setBlogs] = useState([]);
-  const data = [
-    {
-      id: '1',
-      title: 'Software Rekomendasi Pembuat Aplikasi Android Terbaik',
-      category: 'Mobile Dev',
-      createdAt: '01 Januari 2025',
-      image:
-        'https://images.unsplash.com/photo-1618388810903-840bb0d15ea5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D',
-    },
-    {
-      id: '2',
-      title: 'Cara Mudah Menjadi Web Developer Handal',
-      category: 'Web Dev',
-      createdAt: '04 May 2025',
-      image:
-        'https://images.unsplash.com/photo-1688380692117-63178554d76d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE2fHxwcm9ncmFtbWVyfGVufDB8fDB8fHww',
-    },
-    {
-      id: '3',
-      title: 'SQL : Pengertian, Sejarah dan Jenis Perintah SQL',
-      category: 'Web Dev',
-      createdAt: '05 May 2025',
-      image:
-        'https://images.unsplash.com/photo-1534665482403-a909d0d97c67?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D',
-    },
-    {
-      id: '4',
-      title: 'Perbedaan Antara Front End dan Back End Developer',
-      category: 'Web Dev',
-      createdAt: '05 May 2025',
-      image:
-        'https://plus.unsplash.com/premium_photo-1664301973841-fe2da7427d6c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwcm9ncmFtbWVyfGVufDB8fDB8fHww',
-    },
-    {
-      id: '5',
-      title: 'Tips Membuat Website Agar Banyak Pengunjung',
-      category: 'Tips & Trick',
-      createdAt: '05 May 2025',
-      image:
-        'https://images.unsplash.com/photo-1513258496099-48168024aec0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D',
-    },
-  ];
+ 
   useEffect(() => {
     setBlogs(data);
     // eslint-disable-next-line react-hooks/exhaustive-deps
--- fe/src/pages/DetailBlogpage.jsx
+++ fe/src/pages/DetailBlogpage.jsx
@@ -1,9 +1,13 @@
+import { useParams } from 'react-router';
 import Layouts from '../components/Layouts/Layouts';
 import { Box, Grid, GridItem, Text, Image, Button } from '@chakra-ui/react';
 const DetailBlogpage = () => {
+  const { blogId } = useParams();
   return (
     <Layouts>
-      <Box mt={3}></Box>
+      <Box mt={3}>
+        <Image></Image>
+      </Box>
     </Layouts>
   );
 };
