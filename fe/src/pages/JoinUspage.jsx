cb657434e8221b1025a59414d49a896084af5811
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-25 00:34:57 +0700

    add joinuspage

--- fe/src/pages/JoinUspage.jsx
+++ fe/src/pages/JoinUspage.jsx
@@ -1,8 +1,64 @@
-import { Box, Grid, GridItem, Image, Text, Stack, Flex, Button, Group } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+  Stack,
+  Flex,
+  Button,
+  Group,
+  Span,
+} from '@chakra-ui/react';
 import Layouts from '../components/Layouts/Layouts';
 import { Link } from 'react-router';
 import AvatarCard from '../components/Elements/Avatar';
+import { MdOutlineAccessTime } from 'react-icons/md';
+import { HiOutlineSupport } from 'react-icons/hi';
+import { PiGift } from 'react-icons/pi';
+import { GrMoney } from 'react-icons/gr';
+import { SlBadge } from 'react-icons/sl';
+import { IoIosPeople } from 'react-icons/io';
+import IconColor from '../components/Elements/IconButton';
 const JoinUspage = () => {
+  const profits = [
+    {
+      title: 'Mengontrol waktu selalu',
+      description:
+        'Kami tidak membutuhkan anda untuk mematuhi waktu, jadwal, atau sejumlah tempat bimbingan. Anda memutuskan seperti apa tawaran Anda dan bagaimana Anda menyusun bimbingan',
+      icon: MdOutlineAccessTime,
+    },
+    {
+      title: 'Semua dukungan yang anda butuhkan',
+      description:
+        'Terlepas dari sumber daya dan tim dukungan kami, 1.000+ komunitas mentoring yang kuat selalu dengan senang hati membantu menjawab pertanyaan dan kekhawatiran',
+      icon: HiOutlineSupport,
+    },
+    {
+      title: 'Hadiah, fasilitas dan kolaborasi',
+      description:
+        'Dengan mentor di semua sisi industri, kami mengerjakan peluang bersama, memulai proyek, melakukan rujukan pekerjaan, berbagi pengetahuan. Bergabunglah dan jadilah bagian dari apa yang kami lakukan!',
+      icon: PiGift,
+    },
+    {
+      title: 'Uangkan keahlian Anda',
+      description:
+        'Ini perasaan yang baik untuk mendapatkan bayaran untuk membuat perbedaan. Tahukah Anda bahwa program mentoring berbayar memiliki tingkat keberhasilan hingga lima kali lebih tinggi daripada program gratis/sukarela?',
+      icon: GrMoney,
+    },
+    {
+      title: 'Tunjukan pencapaian Anda',
+      description:
+        'Profil mentor Anda tidak hanya mencakup paket pemesanan mentor Anda, tetapi juga testimonial terverifikasi, postingan blog, dan data kinerja. Cara sempurna untuk menampilkan perkembangan Anda sebagai pemimpin.',
+      icon: SlBadge,
+    },
+    {
+      title: 'Dunia Belajar Tanpa Batas',
+      description:
+        'Mentorku adalah program beragam yang menjangkau hampir 100 negara asal, mulai dari pendiri startup tahap awal hingga insinyur utama di Amazon dan desainer pemenang penghargaan.',
+      icon: IoIosPeople,
+    },
+  ];
   return (
     <>
       <Box backgroundColor="textGreen">
@@ -40,16 +96,58 @@ const JoinUspage = () => {
         </Layouts>
       </Box>
       <Layouts>
-        <Stack rowGap={5} mt={10} align={'center'}>
-          <Text fontSize={'xl'} textAlign={'center'} color={'gray.600'}>
+        <Stack
+          rowGap={5}
+          mt={10}
+          align={'center'}
+          backgroundColor={'gray.100'}
+          p={5}
+          rounded={'xl'}
+        >
+          <Text fontSize={'xl'} textAlign={'center'} color={'gray.800'}>
             "Saya Mendapat beberapa panggilan telephone dengan Mentee minggu ini, dan setiap orang
             dari mereka sangat berwawasan, ceria, membangikan semangat dan penuh dengan pertukaran
             ide."
           </Text>
           <Group>
-            <AvatarCard image="https://images.unsplash.com/photo-1742119971773-57e0131095b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fGVudGVwcmVuZXVyfGVufDB8fDB8fHww" />
+            <AvatarCard
+              image="https://images.unsplash.com/photo-1742119971773-57e0131095b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fGVudGVwcmVuZXVyfGVufDB8fDB8fHww"
+              rounded="full"
+            />
+            <Flex columnGap={2} fontSize={'lg'}>
+              <Text>John Doe</Text>
+              <Span fontWeight={'bold'}>/</Span>
+              <Text color={'gray.600'}>Product Designer</Text>
+            </Flex>
           </Group>
         </Stack>
+        <Box mt={14}>
+          <Stack rowGap={3} textAlign={'center'}>
+            <Text fontSize={'xl'}>Mentor On Demand</Text>
+            <Text fontSize={'3xl'} fontWeight={'semibold'}>
+              Mari Mulai bersama, kita disini untuk mendukung anda
+            </Text>
+            <Text fontSize={'2xl'}>
+              Bermitra dengan MentorKu berarti Anda tidak perlu mengurus apapun selain yang <br />
+              paling anda pedulikan
+            </Text>
+          </Stack>
+          <Grid templateColumns={'repeat(2, 1fr)'} templateRows={'repeat(3, 1fr)'} gap={5} mt={5}>
+            {profits.map((profit, index) => (
+              <GridItem key={index} colSpan={1} rowSpan={1} p={3}>
+                <Group columnGap={5} alignItems={'start'}>
+                  <Box backgroundColor={'textGreen'} width={'fit-content'} p={2} rounded={'xl'}>
+                    <profit.icon size={45} color="white" />
+                  </Box>
+                  <Box>
+                    <Text fontSize={'xl'}>{profit.title}</Text>
+                    <Text color={'gray.600'}>{profit.description}</Text>
+                  </Box>
+                </Group>
+              </GridItem>
+            ))}
+          </Grid>
+        </Box>
       </Layouts>
     </>
   );
