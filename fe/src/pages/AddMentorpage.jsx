79f21d4fa6a78b0f62cd0eed8ca93eb09b411ad4
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-25 11:10:28 +0700

    add page addMentor

--- fe/src/pages/AddMentorpage.jsx
+++ fe/src/pages/AddMentorpage.jsx
@@ -1,9 +1,25 @@
 import Layouts from '../components/Layouts/Layouts';
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+  Stack,
+  Flex,
+  Button,
+  Group,
+  Span,
+} from '@chakra-ui/react';
 const AddMentorpage = () => {
   return (
     <Layouts>
-      <Box>add mentor</Box>
+      <Box color={'gray.600'}>
+        <Text fontSize={'3xl'} fontWeight={'semibold'}>
+          Apply as a Mentor
+        </Text>
+        <Box></Box>
+      </Box>
     </Layouts>
   );
 };
