20075987d819247350099e83ae5e87c14ba4bf7d
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-25 19:59:13 +0700

    add formpage

--- fe/src/components/Elements/FormInput/index.jsx
+++ fe/src/components/Elements/FormInput/index.jsx
@@ -1,10 +1,20 @@
 import { Field, Input } from '@chakra-ui/react';
 
-const FormInput = ({ label, placeholder }) => {
+const FormInput = ({ label, placeholder, required = false }) => {
+  return (
+    <Field.Root required={required}>
+      <Field.Label>{label}</Field.Label>
+      <Input placeholder={placeholder} />
+      <Field.ErrorText>This is an error text</Field.ErrorText>
+    </Field.Root>
+  );
+};
+
+export const TextAreaForm = ({ label, placeholder }) => {
   return (
     <Field.Root>
       <Field.Label>{label}</Field.Label>
-      <Input placeholder={placeholder} />
+      <Textarea placeholder={placeholder} />
     </Field.Root>
   );
 };
