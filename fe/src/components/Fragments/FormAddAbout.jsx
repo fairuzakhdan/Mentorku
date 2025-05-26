5f1bf60aa117a03e82db55b06d5e79ec6e05ea44
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-26 09:38:38 +0700

    add page form about mentor

---
+++ fe/src/components/Fragments/FormAddAbout.jsx
@@ -0,0 +1,11 @@
+import { Box, Flex, Stack, Group } from '@chakra-ui/react';
+import FormInput from '../Elements/FormInput';
+const FormAddAboutMentor = () => {
+  return (
+    <Box>
+      <Text>Harap pastikan bahwa pengalaman Anda sesuai dengan keahlian yang Anda miliki</Text>
+    </Box>
+  );
+};
+
+export default FormAddAboutMentor;
