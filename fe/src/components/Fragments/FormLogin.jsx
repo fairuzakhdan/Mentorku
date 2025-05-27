7b5511980dfd4ada48d56e1e41901e89d14155cb
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-27 11:40:25 +0700

    add loginpage

---
+++ fe/src/components/Fragments/FormLogin.jsx
@@ -0,0 +1,30 @@
+import { Stack, Field, Button } from '@chakra-ui/react';
+import FormInput from '../Elements/FormInput';
+import useInput from '../../hooks/useInput';
+import { PasswordInput } from '@/components/ui/password-input';
+import { Check } from '../Elements/FormInput';
+const FormLogin = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  return (
+    <Stack rowGap={5}>
+      <FormInput
+        type="email"
+        label={'Email'}
+        placeholder={'example@gmail.com'}
+        value={email}
+        onChange={onChangeEmail}
+        required
+      />
+      <FormInput
+        type="password"
+        label={'Password'}
+        placeholder={'********'}
+        value={password}
+        onChange={onChangePassword}
+        required
+      />
+    </Stack>
+  );
+};
+export default FormLogin;
