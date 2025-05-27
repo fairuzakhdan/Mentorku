445b57d59d33c93645348d52548087f3f4aedd69
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-28 01:16:50 +0700

    add role mentor

---
+++ fe/src/mentors/pages/UsersMentorpage.jsx
@@ -0,0 +1,11 @@
+import Sidebar from '../../shared/components/Sidebar';
+import { Text } from '@chakra-ui/react';
+const UsersMentorpage = () => {
+  return (
+    <Sidebar type={'mentor'}>
+      <Text color={'textBlue'}>Mentor</Text>
+    </Sidebar>
+  );
+};
+
+export default UsersMentorpage;
