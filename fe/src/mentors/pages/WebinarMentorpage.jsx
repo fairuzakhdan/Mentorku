130fe54da23ea46e3319ad995d25e1c41d5948db
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-29 09:12:54 +0700

    add pagewebinar for mentor

---
+++ fe/src/mentors/pages/WebinarMentorpage.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../shared/components/Sidebar"
+import TableArea from "../../components/Fragments/Table"
+const WebinarMentorpage = () => {
+    const headers = ['Topic','LinkWebinar','Date','Time','Description']
+    return (
+        <Sidebar type={'mentor'}>
+            <TableArea headers={headers}></TableArea>
+        </Sidebar>
+    )
+}
+
+export default WebinarMentorpage
