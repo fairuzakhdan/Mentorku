830d63707cba8df1583e1eb033ec1ad4a96b3ed8
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-29 11:48:51 +0700

    add pagewebinar for mentor

---
+++ fe/src/mentors/pages/DetailUsersMentorpage.jsx.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "../../shared/components/Sidebar";
+const DetailUsersMentorpage = () => {
+    return (
+        <Sidebar type={'mentor'}>
+            <Text>Detail Users</Text>
+        </Sidebar>
+    )
+};
+
+export default DetailUsersMentorpage;
