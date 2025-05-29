1225274e8eab755229add27f808d3cf2dae6ae91
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-29 15:53:34 +0700

    add pagewebinar for mentor

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -27,6 +27,7 @@ import SessionMentorpage from './mentors/pages/SessionsMentorpage';
 import WebinarMentorpage from './mentors/pages/WebinarsMentorpage';
 import BlogMentorpage from './mentors/pages/BlogMentorpage';
 import DetailUsersMentorpage from './mentors/pages/DetailUsersMentorpage.jsx';
+import DetailLessonMentorpage from './mentors/pages/DetailLessonMentorpage.jsx';
 
 const App = () => {
   const [authUser, setAuthUser] = useState(null);
@@ -75,6 +76,7 @@ const App = () => {
           <Route element={<UsersMentorpage />} path="/mentees" />
           <Route element={<DetailUsersMentorpage />} path="/mentees/:userId" />
           <Route element={<LessonsMentorpage />} path="/mylessons" />
+          <Route element={<DetailLessonMentorpage />} path="/mylessons/:lessonId" />
           <Route element={<SessionMentorpage />} path="/sessions" />
           <Route element={<WebinarMentorpage />} path="/webinar" />
           <Route element={<Errorpage />} path={'*'} />
