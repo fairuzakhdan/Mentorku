6d6a107370e44b85f7f8fb5a8b2f91d9f77fa44e
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-30 22:12:24 +0700

    add blogpage for mentor role

---
+++ fe/src/mentors/pages/AddBlogMentorpage.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../shared/components/Sidebar";
+
+const AddBlogMentorpage = () => {
+    return (
+        <Sidebar type={'mentor'}>
+            <FormAddBlog />
+        </Sidebar>
+    );
+};
+
+export default AddBlogMentorpage;
