1480a149ff4d9183b70d7eb2fb35de2eec961003
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-30 14:47:29 +0700

    add form edit lesson

---
+++ fe/src/mentors/components/Forms/FormAddSession.jsx
@@ -0,0 +1,10 @@
+import { Box, Stack, Flex,Group, Button } from "@chakra-ui/react"
+import FormInput from "../../../components/Elements/FormInput"
+const FormAddSession = () => {
+    return (
+        <Stack rowGap={3}>
+
+        </Stack>
+    )
+}
+export default FormAddSession
