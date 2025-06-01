8dbefc4fdb159742fed4bca75ad57664fc7ac5b2
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-06-01 19:59:35 +0700

    add tableadminuser

---
+++ fe/src/admin/pages/EditusersAdmin.jsx
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router';
+import Sidebar from '../../shared/components/Sidebar';
+import FormUsersAdmin from '../components/Forms/FormUsersAdmin';
+import { itemUser } from '../utils/admin';
+import { useState, useEffect } from 'react';
+import { Box } from '@chakra-ui/react';
+const EditUsersAdminpage = () => {
+  const { userId } = useParams();
+
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const userData = itemUser.find((item) => item.id === userId);
+    setUser(userData);
+  }, [userId]);
+  //   console.log(user);
+  return (
+    <Sidebar type={'admin'}>
+      <Box color={'textBlue'}>
+        <FormUsersAdmin type="edit" onSubmit={(data) => console.log(data)} initialData={user} />
+      </Box>
+    </Sidebar>
+  );
+};
+
+export default EditUsersAdminpage;
