733042482eca8572ea02f72b044b614e3a08c16b
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-06-01 17:12:31 +0700

    add tableadminuser

---
+++ fe/src/admin/components/Forms/FormUsersAdmin.jsx
@@ -0,0 +1,14 @@
+import FormInput from '../../../components/Elements/FormInput';
+import { Box, Flex, Stack, Group, Button } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import useInput from '../../../hooks/useInput';
+
+const FormUsersAdmin = () => {
+    const [email, onChangeEmail, setEmail] = useInput('')
+    return (
+        <Box>
+
+        </Box>
+    )
+}
+export default FormUsersAdmin
