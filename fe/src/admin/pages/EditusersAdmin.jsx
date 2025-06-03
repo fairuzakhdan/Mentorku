cb6d6efd7fb78070b83afde2f103dec750799831
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-06-03 15:13:36 +0700

    add formmentor for admin

--- fe/src/admin/pages/EditusersAdmin.jsx
+++ fe/src/admin/pages/EditusersAdmin.jsx
@@ -12,7 +12,7 @@ const EditUsersAdminpage = () => {
     const userData = itemUser.find((item) => item.id === userId);
     setUser(userData);
   }, [userId]);
-  //   console.l
+  //   console
   return (
     <Sidebar type={'admin'}>
       <Box color={'textBlue'}>
