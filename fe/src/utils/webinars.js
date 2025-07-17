9a4f3be4dd42032438582d037eae2647c4aad76e
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-17 13:12:09 +0700

    add fetch webinars api

---
+++ fe/src/utils/webinars.js
@@ -0,0 +1,10 @@
+import { api, fetchWithToken } from './api';
+
+const getAllWebinarByPayment = async () => {
+  const response = await fetchWithToken(`${api}/webinars/success`);
+  const responseJson = await response.json();
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null };
+  }
+  return { error: false, data: responseJson.data };
+};
