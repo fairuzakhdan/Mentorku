3454b5d2c863d9b8aaef58a29217d197ec628fea
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-19 23:13:53 +0700

    update controller lesson

--- be/controllers/payment.js
+++ be/controllers/payment.js
@@ -1,7 +1,7 @@
 const midtransClient = require("midtrans-client");
 const Payment = require("../models/payment");
 
-// Setup 
+// Setup
 const snap = new midtransClient.Snap({
   isProduction: false,
   serverKey: process.env.SERVER_KEY_MIDTRANS,
