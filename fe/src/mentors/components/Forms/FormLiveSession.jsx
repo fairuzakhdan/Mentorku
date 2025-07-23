f39d664bc174b9a4fe97937fb4e7e19d1c3514c4
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-23 22:54:02 +0700

    add live session component

--- fe/src/mentors/components/Forms/FormLiveSession.jsx
+++ fe/src/mentors/components/Forms/FormLiveSession.jsx
@@ -1,13 +1,13 @@
 import FormInput from '../../../components/Elements/FormInput';
 import { Flex, Box, Stack, Button } from '@chakra-ui/react';
 import useInput from '../../../hooks/useInput';
-const FormLiveSession = ({ type = 'add' }) => {
+const FormLiveSession = ({ initialData }) => {
   const [schedule, onChangeScheduleType, setScheduleType] = useInput('');
   const [meetingWeek, onChangeMeetingWeek, SetMeetingWeek] = useInput('');
   return (
-    <Stack rowGap={5} bgColor={'white'} p={5} mt={5} rounded={'xl'}>
+    <Stack rowGap={5} bgColor={'white'} p={5} mt={5} rounded={'xl'} shadow={'sm'}>
       <FormInput
-        label={type === 'add' ? 'Add ScheduleType' : 'Edit ScheduleType'}
+        label={initialData.length === 0 ? 'Add ScheduleType' : 'Edit ScheduleType'}
         required
         value={schedule}
         onChange={onChangeScheduleType}
@@ -15,14 +15,16 @@ const FormLiveSession = ({ type = 'add' }) => {
         type="text"
       />
       <FormInput
-        label={type === 'add' ? 'Add Meeting/Week' : 'Edit Meeting/Week'}
+        label={initialData.length === 0 ? 'Add Meeting/Week' : 'Edit Meeting/Week'}
         required
         value={meetingWeek}
         onChange={onChangeMeetingWeek}
         placeholder={'2'}
         type="number"
       />
-      <Button>Add Schedule</Button>
+      <Button colorPalette={'teal'}>
+        {initialData.length === 0 ? 'Add Schedule' : 'Edit Schedule'}
+      </Button>
     </Stack>
   );
 };
