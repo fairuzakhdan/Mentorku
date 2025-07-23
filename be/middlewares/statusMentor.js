742c781443cc44cdbc33904fbf9d3f82c5cf3776
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-23 16:53:49 +0700

    updated

--- be/middlewares/statusMentor.js
+++
@@ -1,6 +0,0 @@
-const Mentor = require("../models/mentors");
-const statusMentor = async (req, res, next) => {
-  await Mentor.find({ accessLevel: "mentor" });
-  next();
-};
-module.exports = statusMentor;
