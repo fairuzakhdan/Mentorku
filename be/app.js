969634325b41634befc1ece4a8356f58a2156aba
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-26 22:51:13 +0700

    updatd

--- be/app.js
+++ be/app.js
@@ -47,7 +47,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // render the error pag
   res.status(err.status || 500);
   res.send("Page not found");
 });
