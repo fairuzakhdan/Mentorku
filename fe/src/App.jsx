a571e97cdaefcf62c5c2f51969e85ffb9a093340
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-07-31 21:01:08 +0700

    updated

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,4 +1,4 @@
-// App.jsx (Clean Code Ver
+// App.jsx (Clean Code
 import React, { useState, useEffect, useMemo } from 'react';
 import { Routes, Route, useNavigate, Navigate } from 'react-router';
 import '@fontsource/poppins/400.css';
