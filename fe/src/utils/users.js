245f38adb66460ffead15f4c46bba656a6b2f187
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-08-07 22:22:35 +0700

    updated

--- fe/src/utils/users.js
+++ fe/src/utils/users.js
@@ -43,7 +43,7 @@ export const updateUserById = async ({ userId, body }) => {
     body: JSON.stringify(body),
   });
   const responseJson = await response.json();
-  //   console.log(resp
+  //   console.log(res
   if (responseJson.status !== 'success') {
     return { error: true, data: null, message: responseJson.message };
   }
