c7dfb5418323129da1bbd6b0a3e0742126df2617
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-08-14 23:20:45 +0700

    updated

--- be/seeds/mentors.js
+++ be/seeds/mentors.js
@@ -224,7 +224,7 @@ async function seedMentor() {
     {
       name: "Vivek Parmar",
       email: "vivekparmar@gmail.com",
-      phone: "082711212",
+      phone: "0827141212",
       password: "vivek123",
       role: "DevOps Engineer",
       expertise: ["DevOps", "Deployement", "Cloud", "Backend Developer"],
