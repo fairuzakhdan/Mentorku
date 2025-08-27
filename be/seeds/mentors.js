df32e41f196d185ebec9973dfac8dee0a02a8341
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-08-27 22:44:54 +0700

    updated

--- be/seeds/mentors.js
+++ be/seeds/mentors.js
@@ -805,7 +805,7 @@ async function seedMentor() {
     {
       name: "Krunalkumar Shah",
       email: "krunalkumarshah@gmail.com",
-      phone: "0827161212",
+      phone: "08271612712",
       password: "krunalkumar123",
       role: "Teaching Assistant",
       expertise: ["Cyber security ", "Penetration Testing"],
